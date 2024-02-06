1a507e0fd8ec126601156e9decb440a43de52077
tvigger <tvigmaster@mail.ru>
2024-02-06 18:38:45 +0300

    Модифицировал и закомментил.

--- get_image.py
+++ get_image.py
@@ -3,14 +3,20 @@ from PIL import Image
 from io import BytesIO
 
 
-def get_image(ll, spn):
+def get_image(ll: str, spn: str, appearance: str):
+    """
+    Генерирует изображение заданной территории
+    :param ll: Ширина и долгота.
+    :param spn: Угловые размеры.
+    :param appearance: Слой.
+    """
     address = f'https://static-maps.yandex.ru/1.x/'
     par = {
         'll': ll,
         'spn': spn,
-        'l': 'map',
+        'l': appearance,
     }
     im = Image.open(BytesIO(requests.get(address, par).content))
+    # Сохраняем в map.png
     im.save('map.png')
     im.close()
-
