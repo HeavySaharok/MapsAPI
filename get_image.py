932aaa114aa784db7e17bc20a33d200c0c585f40
tvigger <tvigmaster@mail.ru>
2024-02-06 19:04:34 +0300

    Доделал

--- get_image.py
+++ get_image.py
@@ -10,11 +10,17 @@ def get_image(ll: str, spn: str, appearance: str):
     :param spn: Угловые размеры.
     :param appearance: Слой.
     """
+    if appearance == 'satellite':
+        vid = 'sat'
+    elif appearance == 'scheme':
+        vid = 'map'
+    else:
+        vid = 'sat,skl'
     address = f'https://static-maps.yandex.ru/1.x/'
     par = {
         'll': ll,
         'spn': spn,
-        'l': appearance,
+        'l': vid
     }
     im = Image.open(BytesIO(requests.get(address, par).content))
     # Сохраняем в map.png
